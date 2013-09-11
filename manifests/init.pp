55a1b1b0d77800d001fce1cfc1287d76a17ee9c4
Elias Probst <mail@eliasprobst.eu>
2013-09-11 17:48:56 +0200

    Added case for Ubuntu.

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,7 @@
 class tsm::client {
     case $operatingsystem {
         /CentOS|Redhat/ : { include tsm::client::centos }
+        /Ubuntu/ : { include tsm::client::ubuntu }
         default : {}
     }
     include tsm::client::common
