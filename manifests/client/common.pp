4cdaff706de60319bbfa2f6094c77c16ea9a9a1e
Elias Probst <mail@eliasprobst.eu>
2013-09-11 19:00:23 +0200

    Replaced double quotes with single quotes.

--- manifests/client/common.pp
+++ manifests/client/common.pp
@@ -1,49 +1,49 @@
 class tsm::client::common {
 
-    service { "dsmcad":
-        subscribe   => [ File["/opt/tivoli/tsm/client/ba/bin/dsm.sys"], File["/opt/tivoli/tsm/client/ba/bin/dsm.opt"] ],
+    service { 'dsmcad':
+        subscribe   => [ File['/opt/tivoli/tsm/client/ba/bin/dsm.sys'], File['/opt/tivoli/tsm/client/ba/bin/dsm.opt'] ],
         ensure      => running,
         enable      => true,
-        pattern     => "dsmcad",
+        pattern     => 'dsmcad',
     }
 
-    file { "/var/log/tsm":
+    file { '/var/log/tsm':
         ensure => directory,
-        owner  => "root",
-        group  => "root",
+        owner  => 'root',
+        group  => 'root',
     }
 
-    file { "/opt/tivoli/tsm/client/ba/bin/dsm.opt":
-        owner   => "root",
-        group   => "bin",
-        mode    => "0644",
-        require => Package["TIVsm-BA"],
-        content => template("tsmclient/dsm.opt.erb"),
+    file { '/opt/tivoli/tsm/client/ba/bin/dsm.opt':
+        owner   => 'root',
+        group   => 'bin',
+        mode    => '0644',
+        require => Package['TIVsm-BA'],
+        content => template('tsmclient/dsm.opt.erb'),
     }
 
-    file { "/opt/tivoli/tsm/client/ba/bin/dsm.sys":
+    file { '/opt/tivoli/tsm/client/ba/bin/dsm.sys':
         owner   => root,
         group   => bin,
-        mode    => "0644",
-        require => File["/opt/tivoli/tsm/client/ba/bin/dsm.opt"],
-        content => template("tsmclient/dsm.sys.erb"),
+        mode    => '0644',
+        require => File['/opt/tivoli/tsm/client/ba/bin/dsm.opt'],
+        content => template('tsmclient/dsm.sys.erb'),
     }
 
-    file { "/etc/profile.d/dsmcad-paths.sh":
+    file { '/etc/profile.d/dsmcad-paths.sh':
         owner   => root,
         group   => root,
-        mode    => "0644",
-        require => File["/opt/tivoli/tsm/client/ba/bin/dsm.sys"],
-        source => "puppet:///modules/tsmclient/dsmcad-paths.sh",
+        mode    => '0644',
+        require => File['/opt/tivoli/tsm/client/ba/bin/dsm.sys'],
+        source => 'puppet:///modules/tsmclient/dsmcad-paths.sh',
     }
 
     # Check TSM password is valid and update if not
 
-    exec { "store-password":
-        cwd         => "/opt/tivoli/tsm/client/ba/bin",
-        path        => "/opt/tivoli/tsm/client/ba/bin",
-        require     => File["/opt/tivoli/tsm/client/ba/bin/dsm.sys"],
+    exec { 'store-password':
+        cwd         => '/opt/tivoli/tsm/client/ba/bin',
+        path        => '/opt/tivoli/tsm/client/ba/bin',
+        require     => File['/opt/tivoli/tsm/client/ba/bin/dsm.sys'],
         command     => "./dsmc set password $tsmpassword $tsmpassword",
-        onlyif      => "./dsmc query session </dev/null | /bin/grep ^ANS1025E",
+        onlyif      => './dsmc query session </dev/null | /bin/grep ^ANS1025E',
     }
 }
