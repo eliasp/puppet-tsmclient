a65eaa8350f5f78e1aee4aac983b8816b663c2a6
Elias Probst <mail@eliasprobst.eu>
2013-09-18 22:13:48 +0200

    Use puppi's param_lookup() for class params handling.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,8 @@
 class tsmclient (
-    $hostname   = $tsmclient::params::hostname,
-    $nodename   = $tsmclient::params::nodename,
-    $clientport = $tsmclient::params::clientport,
-    $serverport = $tsmclient::params::serverport,
+    $hostname   = params_lookup('hostname'),
+    $nodename   = params_lookup('nodename'),
+    $clientport = params_lookup('clientport'),
+    $serverport = params_lookup('serverport'),
 ) inherits tsmclient::params {
     case $::operatingsystem {
         /CentOS|Redhat/ : { include tsmclient::centos }
