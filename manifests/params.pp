4b72d10de6a121183e1f0b2b3ad799eaeb1434c4
Elias Probst <mail@eliasprobst.eu>
2013-09-18 23:03:18 +0200

    Initial class default params definitions added.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,22 @@
 class tsmclient::params {
-#hostname
-#tsmport
-#tsmserver
-#hostname
+    $servername    = $::fqdn
+    $serverport    = '1500'
+    $serveraddress = ''
+    $clientport    = ''
+    $nodename      = $::fqdn
+    $hostname      = $::fqdn
+    $tsmport       = '1500'
+    $tsmserver     = ''
+    $hostname      = ''
+    $exclude       = {
+        'fs'    => ['/.../'],
+        'dir'   => [
+            '/.../',
+            '/opt/tivoli/tsm/.../',
+        ],
+        'items' => ['/.../'],
+    }
+    $include       = {
+        'items' => [],
+    }
 }
