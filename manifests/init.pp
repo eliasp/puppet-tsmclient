2c81673ec61e97573f5867373c82c3f5dd920f1c
Elias Probst <mail@eliasprobst.eu>
2013-09-18 19:03:51 +0200

    Added further class params.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,8 @@
 class tsmclient (
-    $hostname = $tsmclient::params::hostname,
-    $nodename = $tsmclient::params::nodename,
+    $hostname   = $tsmclient::params::hostname,
+    $nodename   = $tsmclient::params::nodename,
+    $clientport = $tsmclient::params::clientport,
+    $serverport = $tsmclient::params::serverport,
 ) inherits tsmclient::params {
     case $::operatingsystem {
         /CentOS|Redhat/ : { include tsmclient::centos }
